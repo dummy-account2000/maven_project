84282c267155358cd66ed8917a7bbd529b61049b
dummy-account2000 <134507189+dummy-account2000@users.noreply.github.com>
2023-07-20 14:00:27 +0100

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
@@ -42,6 +42,6 @@ public class TestGreeter {
   public void greetShouldIncludeGreetingMessagee() {
     String someone = "World, Thanks you making me Happy!!!";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(lessThan(someone.length())));
   }
 }
